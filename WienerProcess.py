5561ffa17ebf40ee8d79c8f52e8407a1a1dcefb6
nikultsev <nikita.nikultsev@gmail.com>
2024-06-03 21:37:46 +1000

    Push test 2

--- WienerProcess.py
+++ WienerProcess.py
@@ -10,7 +10,9 @@ dtype = [('Date', 'U10'), ('Close/Last', 'f8'),
          ('Open', 'f8'), ('High', 'f8'), ('Low', 'f8'),]
 data = np.genfromtxt('HistoricalData_1717244350919.csv',
                      delimiter=',', dtype=dtype, names=True, encoding='utf-8')
+
 # data = np.transpose(data)
+
 print(data.dtype)
 open_prices = data['Open']
 close_prices = data['CloseLast']
@@ -18,6 +20,8 @@ close_prices = data['CloseLast']
 interlaced = np.empty(
     (open_prices.size + close_prices.size), dtype=open_prices.dtype)
 interlaced[0::2] = open_prices
+
+
 interlaced[1::2] = close_prices
 
 double_date = np.repeat(data['Date'], 2)
