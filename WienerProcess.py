7a393bccf6194c2b6d2d150d230fd806f3357526
nikultsev <nikita.nikultsev@gmail.com>
2024-06-23 17:38:31 +1000

    Added tests and average daily s and p for first year

--- WienerProcess.py
+++ WienerProcess.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import sympy
 from zoom_function import zoom_factory, rebind_pan_to_middle_click
 from derivative_classes import *  # ignores __main__ still
+from tqdm import tqdm
 
 # Plan: test the payout of different (individual or combined) options for a fixed time on the S and P 500 (or other
 # radomly generated ones). I want the ability to set the point in time we are beginning
@@ -31,47 +32,78 @@ def generate_paths(S0, r, sigma, T, M, I):
 
 def main():
 
-    dtype = [('Date', 'U10'), ('Close/Last', 'f8'),
-             ('Open', 'f8'), ('High', 'f8'), ('Low', 'f8')]
-    data = np.genfromtxt('HistoricalData_1717244350919.csv',
-                         delimiter=',', dtype=dtype, names=True, encoding='utf-8')
+    def open_close_average():
+        dtype = [('Date', 'U10'), ('Close/Last', 'f8'),
+                 ('Open', 'f8'), ('High', 'f8'), ('Low', 'f8')]
+        data = np.genfromtxt('HistoricalData_1717244350919.csv',
+                             delimiter=',', dtype=dtype, names=True, encoding='utf-8')
 
-    fig, axs = plt.subplots(2, 3)
+        fig, axs = plt.subplots(2, 3)
 
-    S0 = 100.
-    r = 1
-    sigma = 0.5
-    T = 1.
-    M = 2531
-    I = 10
+        yearly_trading_days = 252
 
-    # print(np.shape(paths))
+        open_prices = data['Open']
+        close_prices = data['CloseLast']
 
-    open_prices = data['Open']
-    close_prices = data['CloseLast']
+        average_daily_prices_10_years = (open_prices + close_prices)/2
 
-    interlaced = np.empty(
-        (open_prices.size + close_prices.size), dtype=open_prices.dtype)
-    interlaced[0::2] = open_prices
-    interlaced[1::2] = close_prices
+        axs[0, 1].plot(np.linspace(1, 252, 251), np.flip(
+            average_daily_prices_10_years[0:yearly_trading_days-1]))
 
-    double_date = np.repeat(data['Date'], 2)
-    interlaced[interlaced == 0] = np.nan
-    print(np.shape(np.flip(interlaced)))
-    S0 = np.flip(interlaced)[0]
+        zoom_factory(axs[0, 1])
+        rebind_pan_to_middle_click()
+        plt.show()
 
-    paths = generate_paths(S0, r, sigma, T, M, I)
-    axs[0, 1].plot(paths[:, :10], linestyle='dashed')
-    axs[0, 1].plot(np.flip(double_date), np.flip(
-        interlaced), label='S&P 500', color='black')
-    print(np.shape(paths[0]))
-    axs[0, 1].set_xticks([])
+        # 
 
-    zoom_factory(axs[0, 1])
-    rebind_pan_to_middle_click()
+        # print(np.shape(open_prices), np.shape(close_prices), np.shape(average_daily_price))
 
-    plt.show()
-    fig.savefig('savedfig.png')
+    open_close_average()
+
+    def open_close_diff():
+        dtype = [('Date', 'U10'), ('Close/Last', 'f8'),
+                 ('Open', 'f8'), ('High', 'f8'), ('Low', 'f8')]
+        data = np.genfromtxt('HistoricalData_1717244350919.csv',
+                             delimiter=',', dtype=dtype, names=True, encoding='utf-8')
+
+        fig, axs = plt.subplots(2, 3)
+
+        # S0 = 100.
+        # r = 1
+        # sigma = 0.5
+        # T = 1.
+        # M = 2531
+        # I = 10
+
+        # print(np.shape(paths))
+
+        yearly_trading_days = 252
+
+        open_prices = data['Open']
+        close_prices = data['CloseLast']
+
+        interlaced = np.empty(
+            (open_prices.size + close_prices.size), dtype=open_prices.dtype)
+        interlaced[0::2] = open_prices
+        interlaced[1::2] = close_prices
+
+        double_date = np.repeat(data['Date'], 2)
+        interlaced[interlaced == 0] = np.nan
+        print(np.shape(np.flip(interlaced)))
+        # S0 = np.flip(interlaced)[0]
+
+        # paths = generate_paths(S0, r, sigma, T, M, I)
+        # axs[0, 1].plot(paths[:, :10], linestyle='dashed')
+        axs[0, 1].plot(np.flip(double_date), np.flip(
+            interlaced), label='S&P 500', color='black')
+        # print(np.shape(paths[0]))
+        axs[0, 1].set_xticks([])
+
+        zoom_factory(axs[0, 1])
+        rebind_pan_to_middle_click()
+
+        plt.show()
+        fig.savefig('savedfig.png')
 
 
 if __name__ == "__main__":
